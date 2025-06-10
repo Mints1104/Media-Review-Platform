1719d7a459843c55f9543d2ed2ddbecfc1223ba6
Mints1104 <harunemretontus@hotmail.com>
2025-06-10 18:34:02 +0100

    Backend: Basic Express server running.

---
+++ server/server.js
@@ -0,0 +1,16 @@
+require('dotenv').config(); // Load environment variables from .env file
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.get('/',(req,res) => {
+    res.send('API is running...');
+});
+
+//Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
