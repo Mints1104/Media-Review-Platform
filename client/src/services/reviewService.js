05e9a76b0ac68c68f9b69dea9f750eeefde6f5e8
Mints1104 <harunemretontus@hotmail.com>
2025-12-01 19:51:28 +0000

    couple minor changes

--- client/src/services/reviewService.js
+++ client/src/services/reviewService.js
@@ -1,42 +1,46 @@
-import axios from 'axios';
-import API_BASE_URL from './api'; 
+import axios from "axios";
+import API_BASE_URL from "./api";
 
-
-const REVIEW_API_URL = `${API_BASE_URL}/api/reviews/`; 
-
+const REVIEW_API_URL = `${API_BASE_URL}/api/reviews/`;
 
 const getReviews = async (params = {}) => {
   const queryString = new URLSearchParams(params).toString();
-  const url = `${REVIEW_API_URL}${queryString ? `?${queryString}` : ''}`; 
+  const url = `${REVIEW_API_URL}${queryString ? `?${queryString}` : ""}`;
   const response = await axios.get(url);
   return response.data;
 };
 
 const getReviewById = async (id) => {
-  const response = await axios.get(REVIEW_API_URL + id); 
+  const response = await axios.get(REVIEW_API_URL + id);
   return response.data;
 };
 
 const createReview = async (reviewData, token) => {
-  const config = { headers: { Authorization: `Bearer ${token}`, }, };
-  const response = await axios.post(REVIEW_API_URL, reviewData, config); 
+  const config = { headers: { Authorization: `Bearer ${token}` } };
+  const response = await axios.post(REVIEW_API_URL, reviewData, config);
+  return response.data;
 };
 
 const updateReview = async (reviewId, reviewData, token) => {
-  const config = { headers: { Authorization: `Bearer ${token}`, }, };
-  const response = await axios.put(REVIEW_API_URL + reviewId, reviewData, config); 
+  const config = { headers: { Authorization: `Bearer ${token}` } };
+  const response = await axios.put(
+    REVIEW_API_URL + reviewId,
+    reviewData,
+    config
+  );
   return response.data;
 };
 
 const deleteReview = async (reviewId, token) => {
-  const config = { headers: { Authorization: `Bearer ${token}`, }, };
-  const response = await axios.delete(REVIEW_API_URL + reviewId, config); 
+  const config = { headers: { Authorization: `Bearer ${token}` } };
+  const response = await axios.delete(REVIEW_API_URL + reviewId, config);
   return response.data;
 };
 
 const getMyReviews = async (token) => {
-  const config = { headers: { Authorization: `Bearer ${token}`, }, };
-  const response = await axios.get(REVIEW_API_URL + 'myreviews', config); 
+  const config = { headers: { Authorization: `Bearer ${token}` } };
+  const response = await axios.get(REVIEW_API_URL + "myreviews", config);
+  return response.data;
 };
 
 const reviewService = {
--- server/models/User.js
+++ server/models/User.js
@@ -1,43 +1,43 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = mongoose.Schema(
-    {
-        username: {
-            type: String,
-            required: true,
-            unique: true,
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-        },
-        password: {
-            type: String,
-            required: true,
-        },
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    {
-        timestamps: true, 
-    }
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
 // Method to compare entered password with hashed password in the database
 userSchema.methods.matchPassword = async function (enteredPassword) {
-    return await bcrypt.compare(enteredPassword, this.password);
+  return await bcrypt.compare(enteredPassword, this.password);
 };
 
 // Pre-save middleware to hash the password before saving a user
-userSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) {
-        next(); // If password is not modified, skip hashing
-    }
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next(); // If password is not modified, skip hashing
+  }
 
-    const salt = await bcrypt.genSalt(10); // Generate a salt
-    this.password = await bcrypt.hash(this.password, salt); // Hash the password
+  const salt = await bcrypt.genSalt(10); // Generate a salt
+  this.password = await bcrypt.hash(this.password, salt); // Hash the password
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
